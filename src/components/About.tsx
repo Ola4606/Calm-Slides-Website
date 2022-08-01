c804084426c358066f4ead57d14a25e1206a11a0
Ola4606 <olaoluwaadeyemo7@gmail.com>
2022-08-01 15:41:25 +0100

    changes to About Component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 function About() {
   return (
-    <div className="pr-32 pt-12 pl-5 pb-5">
+    <div className="px-32 pt-12 pb-5">
       <p className="underline underline-offset-4 font-causal font-extrabold text-right text-2xl decoration-mainColor pb-2">
         What is Calm Slides?
       </p>
-      <p className="font-causal font-bold text-right text-lg">
+      <p className="font-causal font-normal text-right text-lg">
         Calm Slides is a dlnvcksnc scnnc adn adbv dvakbd cbajdb d sc dckjdcajc
+        ajkdbc dcbkjadcb dkjcbjcakb Calm Slides is a dlnvcksnc scnnc adn adbv dvakbd cbajdb d sc dckjdcajc
+        ajkdbc dcbkjadcb dkjcbjcakb Calm Slides is a dlnvcksnc scnnc adn adbv dvakbd cbajdb d sc dckjdcajc
+        ajkdbc dcbkjadcb dkjcbjcakbCalm Slides is a dlnvcksnc scnnc adn adbv dvakbd cbajdb d sc dckjdcajc
+        ajkdbc dcbkjadcb dkjcbjcakb Calm Slides is a dlnvcksnc scnnc adn adbv dvakbd cbajdb d sc dckjdcajc
+        ajkdbc dcbkjadcb dkjcbjcakb Calm Slides is a dlnvcksnc scnnc adn adbv dvakbd cbajdb d sc dckjdcajc
         ajkdbc dcbkjadcb dkjcbjcakb
       </p>
     </div>
--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -3,12 +3,12 @@ import StartBtn from './StartBtn'
 
 function Description() {
   return (
-    <div className='pl-32 pt-12 pr-5 pb-5'>
+    <div className='px-32 pt-12 pb-5'>
         <p className='text-6xl font-title font-semibold text-purple-500'>Listen,</p>
         <p className='text-7xl font-title font-bold text-purple-600'>Watch,</p>
         <p className='text-8xl font-title font-extrabold text-purple-700'>Relax.</p>
 
-        <p className='text-base font-causal font-normal text-black my-7'>View thousands of random images as you relax to calm music...</p>
+        <p className='text-lg font-causal font-normal text-black my-7'>View thousands of random images as you relax to calm music...</p>
 
         <div className='inline-block transform duration-300 hover:scale-110'>
         <StartBtn btnColor="black"/>
